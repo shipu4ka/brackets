709efa7420229db2cba22c70af67af4e83c6a812
grafit.1 <grafit.1@yandex.ru>
2022-02-08 21:57:50 +0300

    feat: add resolution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const strArr = str.split("");
+  const objBrackets = {};
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    objBrackets[bracketsConfig[i][0]] = bracketsConfig[i][1];
+  }
+  const sameBrackets = Object.keys(objBrackets).filter(
+    (key) => key === objBrackets[key]
+  );
+  let i = 0;
+  while (i < strArr.length) {
+    if (sameBrackets.includes(strArr[i]) && strArr[i] === strArr[i + 1]) {
+      strArr.splice(i, 2);
+      i = i - 2;
+    } else if (
+      strArr[i] in objBrackets &&
+      objBrackets[strArr[i]] === strArr[i + 1]
+    ) {
+      strArr.splice(i, 2);
+      i = i - 2;
+    } else {
+      i = i + 1;
+    }
+  }
+  return strArr.length === 0;
+};
